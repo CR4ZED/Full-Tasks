9131da4b083b667ddcbb4f78b1196e0c1ec0c893
CR4ZED <cleanmonk13@gmail.com>
2021-08-12 12:10:07 +0530

    added new slide feature

--- carousel/app.js
+++ carousel/app.js
@@ -1,14 +1,20 @@
 const next = document.querySelector("#next");
 const prev = document.querySelector("#prev");
+const slides = document.getElementById("imgs");
 const imgs = document.querySelector("#imgs").children;
-const indicators = document.querySelector(".indicators").children;
+const indicators = document.querySelector(".indicators");
+const spans = document.querySelector(".indicators").children;
+const add = document.getElementById("add");
+const addNew = document.getElementById("addNew");
+const modal = document.getElementById("modal");
+const imgUrl = document.getElementById("imgUrl");
 
 function nextImg() {
   let [currActiveImg, currActiveSpan] = getCurrentObjects();
 
   if (currActiveImg === imgs[imgs.length - 1]) {
     imgs[0].classList.add("active");
-    indicators[0].classList.add("active");
+    spans[0].classList.add("active");
   } else {
     currActiveImg.nextElementSibling.classList.add("active");
     currActiveSpan.nextElementSibling.classList.add("active");
@@ -20,7 +26,7 @@ function prevImg() {
 
   if (currActiveImg === imgs[0]) {
     imgs[imgs.length - 1].classList.add("active");
-    indicators[indicators.length - 1].classList.add("active");
+    spans[indicators.length - 1].classList.add("active");
   } else {
     currActiveImg.previousElementSibling.classList.add("active");
     currActiveSpan.previousElementSibling.classList.add("active");
@@ -36,7 +42,34 @@ function getCurrentObjects() {
   return [currActiveImg, currActiveSpan];
 }
 
+function addNewImg(e) {
+  e.preventDefault();
+  modal.classList.add("show");
+  const newImg = imgUrl.value;
+  const img = document.createElement("img");
+  img.setAttribute("src", newImg);
+  const span = document.createElement("span");
+
+  slides.appendChild(img);
+  indicators.appendChild(span);
+  imgUrl.value = "";
+  modal.classList.remove("show");
+}
+
+function hideModal(e) {
+  if (e.target.id === "modal") {
+    modal.classList.remove("show");
+  }
+}
+
+function showModal() {
+  modal.classList.add("show");
+}
+
 setInterval(nextImg, 5000);
 
 next.addEventListener("click", nextImg);
 prev.addEventListener("click", prevImg);
+add.addEventListener("click", showModal);
+addNew.addEventListener("click", addNewImg);
+modal.addEventListener("click", hideModal);
