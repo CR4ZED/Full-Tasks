1c161e4460ae93005393c4c4155d8bc48ab5ea0f
CR4ZED <cleanmonk13@gmail.com>
2021-08-13 13:16:49 +0530

    added debounce to form validation

--- debounce/app.js
+++ debounce/app.js
@@ -1,11 +1,14 @@
 const btn1 = document.getElementById("btn1");
 const btn2 = document.getElementById("btn2");
 
-function debounce(func, delay) {
+const name1 = document.getElementById("name1");
+const name2 = document.getElementById("name2");
+
+function debounce(func, delay, args) {
   let id;
   return () => {
     clearTimeout(id);
-    id = setTimeout(func, delay);
+    id = setTimeout(func, delay, args);
   };
 }
 
@@ -13,7 +16,21 @@ function onClick() {
   console.log("clicked");
 }
 
+function validate(input) {
+  if (input.value === "ankush") {
+    input.classList.remove("invalid");
+    input.classList.add("valid");
+  } else {
+    input.classList.remove("valid");
+    input.classList.add("invalid");
+  }
+}
+
 let debouncedOnClick = debounce(onClick, 2000);
+let debouncedValidate = debounce(validate, 500, name2);
 
 btn1.addEventListener("click", onClick);
 btn2.addEventListener("click", debouncedOnClick);
+
+name1.addEventListener("keyup", () => validate(name1));
+name2.addEventListener("keyup", debouncedValidate);
