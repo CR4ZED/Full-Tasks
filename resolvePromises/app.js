ec990935e6375de477b1205ff8bec23de0e94b87
CR4ZED <cleanmonk13@gmail.com>
2021-08-14 12:58:04 +0530

    resolver v3.0

--- resolvePromises/app.js
+++ resolvePromises/app.js
@@ -2,37 +2,53 @@ function generatePromise(data, delay) {
   return new Promise((resolve) => setTimeout(resolve, delay, data));
 }
 
-function checkTimer(promise, delay) {
-  return Promise.race([
-    promise,
-    new Promise((resolve, reject) =>
-      setTimeout(() => reject("rejected"), 5000 - delay)
-    ),
-  ]);
+function checkTimer(promise) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      //throw error if promise takes more than 5s to resolve/reject
+      reject("TIMEOUT");
+    }, 5000);
+    promise
+      .then((data) => {
+        resolve(data);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
 }
 
-async function resolver(promises) {
+function resolver(promises) {
   let res = [];
   for (let promise of promises) {
-    let now = performance.now();
-    setTimeout(() => {
-      checkTimer(promise, (now - start) * 1000)
-        .then((data) => {
-          console.log(data);
-          res.push(data);
-        })
-        .catch((err) => console.log("failed"));
-    });
+    checkTimer(promise)
+      .then((data) => {
+        console.log(data);
+        res.push(data);
+      })
+      .catch((err) => {
+        console.log(`err: ${err}`);
+      });
   }
-  // return res;
+  return res;
 }
-let start = performance.now();
-// console.log(start);
-let promise1 = generatePromise("success p1", 5000);
+
+let promise1 = generatePromise("success p1", 6000);
 let promise2 = generatePromise("success p2", 2000);
 let promise3 = generatePromise("success p3", 5000);
-let promise4 = generatePromise("success p4", 3000);
+let promise4 = generatePromise("success p4", 20000);
 
-resolver([promise1, promise2, promise3, promise4]).then((data) => {
-  console.log(data);
-});
+resolver([promise1, promise2, promise3, promise4]);
+// });
+// let data = "";
+// promise4.then((res) => {
+//   data = res;
+//   console.log(data)
+// });
+// setTimeout(() => {
+//   if (!data) {
+//     delete promise4
+//     console.log("timeout");
+//     throw new Error('timeout')
+//   }
+// }, 5000);
