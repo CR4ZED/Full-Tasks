52d7e0d63af3a3cf7e5717272b42485c6ea2641c
CR4ZED <cleanmonk13@gmail.com>
2021-09-07 12:54:33 +0530

    initial commit

--- myApp/api/Task.js
+++ myApp/api/Task.js
@@ -1,17 +1,18 @@
-const {Datastore} = require('@google-cloud/datastore')
+const { Datastore } = require("@google-cloud/datastore");
 
-const datastore = new Datastore()
+const projectId = "gcp-demo-325209";
 
+const datastore = new Datastore({ projectId });
 
-const kind = 'Task'
+const kind = "Task";
 
-async function createTask(id,task){
-    const key = datastore.key([kind])
-    const taskInfo = {createdBy:id,...task}
-    await datastore.insert({key,data:taskInfo})
-    console.log('saved 1 task')
+async function createTask(id, task) {
+  const key = datastore.key([kind]);
+  const taskInfo = { createdBy: id, ...task };
+  await datastore.insert({ key, data: taskInfo });
+  console.log("saved 1 task");
 }
 
-createTask('user1',{description:'finsih this task',status:false})
+createTask("user1", { description: "finsih this task", status: false });
 
-module.exports = createTask
+module.exports = createTask;
